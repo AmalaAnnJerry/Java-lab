d6c1f5d3166e6bb1d938dbee9f86d52a3b33abcb
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2023-11-18 14:50:22 +0530

    Add files via upload

---
+++ Implement(T).java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Student
+{
+	int rollNumber,mathMarks,phyMarks,chemMarks;
+	String name;
+
+void getRequiredDetails()
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter details:\n");
+	System.out.println("Name:");
+	name=sc.next();
+	System.out.println("Rollno:");
+	rollNumber=sc.nextInt();
+	System.out.println("MathMarks:");
+	mathMarks=sc.nextInt();
+	System.out.println("PhyMarks:");
+	phyMarks=sc.nextInt();
+	System.out.println("ChemMarks:");
+	chemMarks=sc.nextInt();
+}
+
+void displayAverage()
+{
+	double avg;
+	avg=(double)(mathMarks+phyMarks+chemMarks)/3;
+	System.out.println("Average marks: "+avg);
+}
+}
+
+class Implement
+{
+public static void main(String args[])
+{
+	Student S=new Student();
+	S.getRequiredDetails();
+	S.displayAverage();
+}
+}
+
+
