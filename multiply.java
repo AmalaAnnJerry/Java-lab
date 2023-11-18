6a1e93b2ffb985a68d82f0ad160aa82bfe18be2c
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2023-11-18 13:28:48 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class multiply
+{
+	public static void main(String args[])
+	{
+		int a,b,m;
+		System.out.println("Enter 2 numbers");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		m=a*b;
+		System.out.println("product is"+m);
+	}
+}
+		
