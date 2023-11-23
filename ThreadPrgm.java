b86c1206961e1e19c22d34e70d6e9f180381c76b
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2023-11-23 13:43:56 +0530

    Add files via upload

---
+++ ThreadPrgm.java
@@ -0,0 +1,38 @@
+class Odd extends Thread
+{
+public void run()
+{
+for(int i=1;i<=100;i++)
+{
+if(i%2!=0)
+{
+System.out.println("Odd no i:"+i);
+}
+}
+System.out.println("Exiting thread from A");
+}
+}
+class Even extends Thread
+{
+public void run()
+{
+for(int i=1;i<=100;i++)
+{
+if(i%2==0)
+{
+System.out.println("Even no i:"+i);
+}
+}
+System.out.println("Exiting thread from B");
+}
+}
+class ThreadPrgm
+{
+public static void main(String args[])
+{
+Odd.o=new Odd();
+o.start();
+Even.e=new Even();
+e.start();
+}
+}
